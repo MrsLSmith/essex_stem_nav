65036d5ef289b9b39d3bab51088de3661f6654ea
Lea Ann Smith <lsmith@ewsd.org>
2023-10-25 13:11:24 -0400

    before webview

--- lib/tabs/tab1.dart
+++ lib/tabs/tab1.dart
@@ -10,6 +10,7 @@ class Tab1 extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Image.asset('assets/images/stemlarge1.jpg'),
           const Text("Welcome to the STEM App!"),
           Padding(
             padding: const EdgeInsets.only(top: 16.0),
